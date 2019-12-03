31ce30985c642cd298f48a7eeb11a8aef10a2b4d
Admin <you@example.com>
2019-12-03 17:19:45 +0530

    ADD:Sort State And Write to JSON file

--- src/main/java/com/bridgelabz/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/StateCensusAnalyser.java
@@ -1,148 +1,66 @@
 package com.bridgelabz;
 
-import com.opencsv.CSVWriter;
+import com.google.gson.Gson;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import com.opencsv.bean.StatefulBeanToCsv;
-import com.opencsv.bean.StatefulBeanToCsvBuilder;
-import com.opencsv.exceptions.CsvDataTypeMismatchException;
-import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
+
 public class StateCensusAnalyser {
-    public String matchStateCount(int cnt, String filePath, String className, char separator) throws CustomException {
-        int count = 0;
-        try {
+    public List<CSVStateCensus> matchStateCensusCount(int cnt, String filePath, String className, char separator) throws CustomException {
 
-            if (!filePath.contains(".csv")) {
-                throw new CustomException(CustomException.ExceptionType.INVALID_FILETYPE);
-            }
-
-            CsvToBean<CSVStates> csvToBean = this.OpenCSVBuilder(filePath, className, separator);
-            Iterator<CSVStates> csvDataIterator = csvToBean.iterator();
-
-            while (csvDataIterator.hasNext()) {
-                ++count;
-                CSVStates csvState = csvDataIterator.next();
-                System.out.println("SrNo : " + csvState.getSrNo());
-                System.out.println("StateName : " + csvState.getStateName());
-                System.out.println("TIN : " + csvState.getTIN());
-                System.out.println("StateCode : " + csvState.getStateCode());
-                System.out.println("==========================");
-            }
-            if (count == cnt) {
-                return "HAPPY";
-            }
-        } catch (RuntimeException e) {
-            throw new CustomException(CustomException.ExceptionType.CSV_REQUIRED_FIELD_EMPTY_EXCEPTION);
-        }
-
-        return null;
-    }
-
-    public String matchStateCensusCount(int cnt, String filePath, String className, char separator) throws CustomException {
-        int count = 0;
+        List<CSVStateCensus> csvCensusList = null;
         try {
             if (!filePath.contains(".csv")) {
                 throw new CustomException(CustomException.ExceptionType.INVALID_FILETYPE);
             }
-
-            CsvToBean<CSVStateCensus> csvToBean = this.OpenCSVBuilder(filePath, className, separator);
-            Iterator<CSVStateCensus> csvDataIterator = csvToBean.iterator();
-
-            while (csvDataIterator.hasNext()) {
-                ++count;
-                CSVStateCensus csvStateCensus = csvDataIterator.next();
-                System.out.println("State : " + csvStateCensus.getState());
-                System.out.println("Population : " + csvStateCensus.getPopulation());
-                System.out.println("AreaInSqKm : " + csvStateCensus.getAreaInSqKm());
-                System.out.println("DensityPerSqKm : " + csvStateCensus.getDensityPerSqKm());
-                System.out.println("==========================");
-            }
-            if (count == cnt) {
-                return "HAPPY";
-            }
-
-        } catch (RuntimeException e) {
-            throw new CustomException(CustomException.ExceptionType.CSV_REQUIRED_FIELD_EMPTY_EXCEPTION);
-        }
-        return null;
-    }
-
-    public <T> CsvToBean OpenCSVBuilder(String filename, String classname, char separator) throws CustomException {
-
-        CsvToBean<T> csvToBean;
-        try {
-            Reader reader = Files.newBufferedReader(Paths.get(filename));
-            csvToBean = new CsvToBeanBuilder(reader)
-                    .withType(Class.forName(classname))
+            Reader reader = Files.newBufferedReader(Paths.get(filePath));
+            CsvToBean<CSVStateCensus> csvToBean = new CsvToBeanBuilder(reader)
+                    .withType(Class.forName(className))
                     .withIgnoreLeadingWhiteSpace(true)
                     .withSeparator(separator)
                     .build();
-
-            return csvToBean;
-        } catch (NoSuchFileException e) {
+            csvCensusList = csvToBean.parse();
+        }catch (NoSuchFileException e) {
             throw new CustomException(CustomException.ExceptionType.NO_SUCH_FILE);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+        }catch (RuntimeException | IOException | ClassNotFoundException e) {
+            throw new CustomException(CustomException.ExceptionType.CSV_REQUIRED_FIELD_EMPTY_EXCEPTION);
         }
-        return null;
+
+        SortState(csvCensusList);
+        return csvCensusList;
     }
 
-
-    public String matchCensusObjectWithStateCode(int cnt,String stateCodeFileName, String stateCodeClassName, String stateCensusFileName, String stateCensusClassName,String newFileName ,char separator) throws CustomException, CsvRequiredFieldEmptyException, IOException, CsvDataTypeMismatchException {
-        CsvToBean<CSVStateCensus> csvStateCensusToBean = this.OpenCSVBuilder(stateCensusFileName, stateCensusClassName, separator);
-        Iterator<CSVStateCensus> censusIterator = csvStateCensusToBean.iterator();
-        List<StateCensusData> stateCensusDataObjectList = new ArrayList<>();
-
-        while (censusIterator.hasNext()) {
-            CsvToBean<CSVStates> csvStateCodeToBean = this.OpenCSVBuilder(stateCodeFileName, stateCodeClassName, separator);
-            Iterator<CSVStates> csvStateCodeDataIterator = csvStateCodeToBean.iterator();
-            CSVStateCensus csvCensus = censusIterator.next();
-            while (csvStateCodeDataIterator.hasNext()) {
-                CSVStates csvStateCode = csvStateCodeDataIterator.next();
-                if (csvCensus.getState().equals(csvStateCode.getStateName())) {
-                    StateCensusData obj = new StateCensusData();
-                    obj.setSrNo(csvStateCode.getSrNo());
-                    obj.setStateName(csvStateCode.getStateName());
-                    obj.setStateCode(csvStateCode.getStateCode());
-                    obj.setTIN(csvStateCode.getTIN());
-                    obj.setPopulation(csvCensus.getPopulation());
-                    obj.setAreaInSqKm(csvCensus.getAreaInSqKm());
-                    obj.setDensityPerSqKm(csvCensus.getDensityPerSqKm());
-                    stateCensusDataObjectList.add(obj);
-                    break;
+    public void SortState(List<CSVStateCensus> csvCensusList) throws CustomException {
+        for(int i=0;i<csvCensusList.size()-1;i++){
+            for(int j=0;j<csvCensusList.size()-i-1;j++){
+                if(csvCensusList.get(j).getState().compareTo(csvCensusList.get(j+1).getState())>0){
+                    CSVStateCensus tempObj=csvCensusList.get(j);
+                    csvCensusList.set(j,csvCensusList.get(j+1));
+                    csvCensusList.set(j+1,tempObj);
                 }
             }
         }
-        try (
-                Writer writer = Files.newBufferedWriter(Paths.get(newFileName));
-        ) {
-            CSVWriter csvWriter = new CSVWriter(writer,
-                    CSVWriter.DEFAULT_SEPARATOR,
-                    CSVWriter.NO_QUOTE_CHARACTER,
-                    CSVWriter.DEFAULT_ESCAPE_CHARACTER,
-                    CSVWriter.DEFAULT_LINE_END);
-            String[] headerRecord = {"SrNo", "StateName", "StateCode", "TIN", "Population", "AreaInSqKm", "DensityPerSqKm"};
-            csvWriter.writeNext(headerRecord);
-            for (int i = 0; i < stateCensusDataObjectList.size(); i++) {
-                csvWriter.writeNext(new String[]{stateCensusDataObjectList.get(i).getSrNo(), stateCensusDataObjectList.get(i).getStateName(), stateCensusDataObjectList.get(i).getStateCode(), String.valueOf(stateCensusDataObjectList.get(i).getTIN()), String.valueOf(stateCensusDataObjectList.get(i).getPopulation()), String.valueOf(stateCensusDataObjectList.get(i).getAreaInSqKm()), String.valueOf(stateCensusDataObjectList.get(i).getDensityPerSqKm())});
-            }
+        writeToJsonFile(csvCensusList);
+   }
+
+    public void writeToJsonFile(List<CSVStateCensus> list){
+        String filename="/home/admin1/IdeaProjects/IndianStatesCensusAnalyserProblem/src/main/resources/StateCensus.json";
+        Gson gson = new Gson();
+        String json = gson.toJson(list);
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(filename);
+            fileWriter.write(json);
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        if(cnt==stateCensusDataObjectList.size())
-        return "HAPPY";
-
-        return null;
     }
-
 
 }
--- src/test/java/com/bridgelabz/TestCasesIndianStatesCensusAnalyser.java
+++ src/test/java/com/bridgelabz/TestCasesIndianStatesCensusAnalyser.java
@@ -6,6 +6,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 public class TestCasesIndianStatesCensusAnalyser {
     private static final String STRING_ARRAY_SAMPLE = "/home/admin1/IdeaProjects/IndianStatesCensusAnalyserProblem/src/main/resources/StateCodeCensusData.csv";
@@ -13,63 +14,13 @@ public class TestCasesIndianStatesCensusAnalyser {
     private static final String STATE_CENSUS_CSV_FILE_PATH = "/home/admin1/IdeaProjects/IndianStatesCensusAnalyserProblem/src/main/resources/StateCensusData.csv";
     private static  final String STATE_CLASSNAME="com.bridgelabz.CSVStates";
     private static  final String STATE_CENSUS_CLASSNAME="com.bridgelabz.CSVStateCensus";
-    @Test
-    public void GivenStateCSV_NumberOfRecord_ShouldMatchExpected() {
-        StateCensusAnalyser Obj = new StateCensusAnalyser();
-        try {
-            String stateCountResult = Obj.matchStateCount(37,STATE_CSV_FILE_PATH,STATE_CLASSNAME,',');
-            Assert.assertEquals("HAPPY", stateCountResult);
-        } catch (CustomException e) {
-            e.printStackTrace();
-        }
-    }
 
-    @Test
-    public void GivneStateCSV_IncorrectFile_ShouldThrowCustomException() {
-        StateCensusAnalyser obj = new StateCensusAnalyser();
-        try {
-            String s = obj.matchStateCount(37,"/home/admin1/IdeaProjects/IndianStatesCensusAnalyserProblem/src/main/resources/StateCode1.csv","STATE_CLASSNAME",',');
-        }catch (CustomException e) {
-            Assert.assertEquals(CustomException.ExceptionType.NO_SUCH_FILE,e.type);
-        }
-    }
-
-    @Test
-    public void GivenStateCSV_InCorrectFileType_ShouldThrowCustomException() {
-        StateCensusAnalyser obj = new StateCensusAnalyser();
-        try {
-            obj.matchStateCount(37,"/home/admin1/IdeaProjects/IndianStatesCensusAnalyserProblem/src/main/resources/StateCode.json",STATE_CLASSNAME,',');
-        } catch (CustomException e) {
-             Assert.assertEquals(CustomException.ExceptionType.INVALID_FILETYPE,e.type);
-        }
-    }
-
-    @Test
-    public void GivenStateCSV_InCorrectDelimiter_ShouldThrowCustomException() {
-        StateCensusAnalyser obj = new StateCensusAnalyser();
-        try {
-            obj.matchStateCount(37,STATE_CSV_FILE_PATH,STATE_CLASSNAME,';');
-        } catch (CustomException e) {
-            Assert.assertEquals(CustomException.ExceptionType.CSV_REQUIRED_FIELD_EMPTY_EXCEPTION,e.type);
-        }
-    }
-
-    @Test
-    public void GivenStateCSV_InCorrectHeader_ShouldThrowCustomException() {
-        StateCensusAnalyser obj = new StateCensusAnalyser();
-        try {
-            obj.matchStateCount(37,STATE_CSV_FILE_PATH,STATE_CLASSNAME,',');
-        } catch (CustomException e) {
-            Assert.assertEquals(CustomException.ExceptionType.CSV_REQUIRED_FIELD_EMPTY_EXCEPTION,e.type);
-        }
-    }
-    //Following Test cases for stateCensusData
     @Test
     public void GivenStateCensusCSV_NumberOfRecord_ShouldMatchExpected() {
         StateCensusAnalyser obj = new StateCensusAnalyser();
         try {
-            String result= obj.matchStateCensusCount(29,STATE_CENSUS_CSV_FILE_PATH,STATE_CENSUS_CLASSNAME,',');
-            Assert.assertEquals("HAPPY",result);
+            List<CSVStateCensus> result= obj.matchStateCensusCount(29,STATE_CENSUS_CSV_FILE_PATH,STATE_CENSUS_CLASSNAME,',');
+            Assert.assertEquals(29,result.size());
         } catch (CustomException e) {
             e.printStackTrace();
         }
@@ -112,23 +63,7 @@ public class TestCasesIndianStatesCensusAnalyser {
         }
     }
 
-    @Test
-    public void testcase() {
-        StateCensusAnalyser obj=new StateCensusAnalyser();
-        try {
-            String result=obj.matchCensusObjectWithStateCode(28,STATE_CSV_FILE_PATH, STATE_CLASSNAME, STATE_CENSUS_CSV_FILE_PATH, STATE_CENSUS_CLASSNAME,STRING_ARRAY_SAMPLE, ',');
-            Assert.assertEquals("HAPPY",result);
 
-        } catch (CustomException e) {
-            e.printStackTrace();
-        } catch (CsvRequiredFieldEmptyException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (CsvDataTypeMismatchException e) {
-            e.printStackTrace();
-        }
-    }
 
 
 }
